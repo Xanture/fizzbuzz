670ede7a7ae5cc5a715f02933343d1c66b8a0f31
Brian McFarlane <bmcfarlane82@gmail.com>
2017-09-11 16:45:50 -0700

    Add project files.

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userInput = Convert.ToInt32(Console.ReadLine());
+            if (userInput % 3 == 0 && userInput % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+                return;
+            }
+            if (userInput % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+                return;
+            }
+            if (userInput % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+                return;
+            }
+            else
+            {
+                Console.WriteLine(userInput);
+                return;
+            }
+        }
+    }
+}
